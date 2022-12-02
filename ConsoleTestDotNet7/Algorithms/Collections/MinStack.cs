66596091427c3089d76c09ac1b716465ebea99d4
Fernando Ulloa <fjulloap23@gmail.com>
2022-12-01 19:33:59 -0600

    Add MinStack

---
+++ ConsoleTestDotNet7/Algorithms/Collections/MinStack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace ConsoleTestDotNet7.Algorithms.Collections
+{
+	public class MinStack<T> where T : IComparable
+	{
+		public int Count { get; private set; } = 0;
+
+		private T[]? _buffer = default(T[]?);
+
+		public T? Minimum { get; set; }
+
+		public void Push(T element)
+		{
+			if (_buffer == null || _buffer.Length < Count + 1)
+			{
+				RealocateBuffer();
+			}
+
+			_buffer![Count] = element;
+
+			Count += 1;
+
+			if (Minimum == null || Minimum.CompareTo(element) > 0)
+			{
+				Minimum = element;
+			}
+		}
+
+		public void Pop()
+		{
+			if (Count > 0)
+			{
+                Count -= 1;
+				if (Count > 1)
+				{
+					Minimum = _buffer![0];
+                    for (int i = 0; i < Count; ++i)
+                    {
+                        if (Minimum.CompareTo(_buffer[i]) > 0)
+                        {
+                            Minimum = _buffer[i];
+                        }
+                    }
+                }
+				else
+				{
+					Minimum = default(T);
+				}
+            }
+		}
+
+		private void RealocateBuffer()
+		{
+			var length = Count == 0 ? 2 : Count * 2;
+
+            var newBuffer = new T[length];
+			if (_buffer != null)
+			{
+				for (int i = 0; i < _buffer.Length; ++i)
+				{
+					newBuffer[i] = _buffer[i];
+				}
+			}
+
+			_buffer = newBuffer;
+		}
+	}
+}
---
+++ ConsoleTestDotNet7Tests/Algorithms/Collections/MinTests.cs
@@ -0,0 +1,70 @@
+﻿using ConsoleTestDotNet7.Algorithms.Collections;
+
+namespace ConsoleTestDotNet7Tests.Algorithms.Collections
+{
+	public class MinTests
+	{
+		[Fact]
+		public void PushTest()
+		{
+			MinStack<int> stack = new();
+			stack.Push(12);
+            stack.Push(5667);
+			stack.Push(2);
+            stack.Push(-1);
+
+            Assert.Equal(4, stack.Count);
+			Assert.Equal(-1, stack.Minimum);
+        }
+
+        [Fact]
+        public void DefaultStateTest()
+        {
+            MinStack<int> stack = new();
+            Assert.Equal(0, stack.Minimum);
+        }
+
+        [Fact]
+        public void PopTest()
+        {
+            MinStack<int> stack = new();
+            stack.Push(12);
+            stack.Push(5667);
+            stack.Push(2);
+            stack.Push(-1);
+
+            Assert.Equal(4, stack.Count);
+
+            stack.Pop();
+
+            Assert.Equal(3, stack.Count);
+
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+
+            Assert.Equal(0, stack.Count);
+
+            stack.Pop();
+
+            Assert.Equal(0, stack.Count);
+        }
+
+        [Fact]
+        public void MinimumTest()
+        {
+            MinStack<int> stack = new();
+            stack.Push(12);
+            stack.Push(5667);
+            stack.Push(2);
+            stack.Push(-1);
+
+            Assert.Equal(-1, stack.Minimum);
+
+            stack.Pop();
+
+            Assert.Equal(2, stack.Minimum);
+        }
+    }
+}
+
